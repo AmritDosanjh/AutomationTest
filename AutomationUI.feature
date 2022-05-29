958ce8b1dd701d830648f8a47601b517fd46f37e
Man <Man@DESKTOP-VC84VCT>
2022-05-29 22:51:11 +0100

    Add project files.

---
+++ AutomationUI.feature
@@ -0,0 +1,7 @@
+﻿Feature: AutomationUI
+
+This is a set of AutomationUI Tests
+
+@UI_Test
+Scenario: GalacticoElevenCreateLeague
+	Given The user navigates to Galactico eleven
---
+++ Steps.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace AutomationTest
+{
+    [Binding]
+    public class Steps
+    {
+        [Given(@"The user navigates to Galactico eleven")]
+        public void GivenSomeGeezerGoesToGoogle()
+        {
+            IWebDriver driver = new ChromeDriver(".");
+            driver.Navigate().GoToUrl("https://www.galacticoeleven.com/#!/login");
+
+            Thread.Sleep(5000);
+
+            IWebElement loginInInput = driver.FindElement(By.XPath("//input[@type='email']"));
+            IWebElement passwordInInput = driver.FindElement(By.XPath("//input[@type='password']"));
+            IWebElement loginButton = driver.FindElement(By.XPath("//button[@ng-click='login()']"));
+
+            loginInInput.SendKeys("heiiigssxltxobiujl@bvhrs.com");
+            passwordInInput.SendKeys("Mortgagesupport1");
+            loginButton.Click();
+        }
+    }
+}
